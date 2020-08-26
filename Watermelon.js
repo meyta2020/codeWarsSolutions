5e0af41d3feddf3c9ac9bcac0337c9bf8125acaa
meyta2020 <tmeysner@gmail.com>
2020-08-26 13:16:16 -0700

    Codewars solution "Watermelon"

---
+++ Watermelon.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/55192f4ecd82ff826900089e
+
+function divide(weight){
+    if (weight > 2 && weight % 2 === 0){
+        return true;
+    } else {
+        return false;
+    }
+}
