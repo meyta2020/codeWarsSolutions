d68379641b3bad0e31075bd640f3c03be2c1641d
meyta2020 <tmeysner@gmail.com>
2020-09-02 10:33:43 -0700

    Codewars solution "Determine offspring sex based on genes XX and XY chromosomes"

---
+++ Determine offspring sex based on genes XX and XY chromosomes.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/56530b444e831334c0000020
+
+function chromosomeCheck(sperm) {
+    if (sperm === "XX"){
+        return "Congratulations! You're going to have a daughter.";
+    } else {
+        return "Congratulations! You're going to have a son.";
+    }
+}
