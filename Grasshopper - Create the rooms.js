d1f7b9d7a8c91f057b52ca7e1707660addbca22b
meyta2020 <tmeysner@gmail.com>
2020-10-01 09:28:11 -0700

    Codewars solution "Grasshopper - Create the rooms"

---
+++ Grasshopper - Create the rooms.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/56a29b237e9e997ff2000048/train/javascript
+
+var rooms = {
+    room1: {
+        name: 'First',
+        description: 'fan',
+        completed: 'true'
+    },
+    room2: {
+        name: 'Second',
+        description: 'fan',
+        completed: 'true'
+    },
+    room3: {
+        name: 'Third',
+        description: 'fan',
+        completed: 'true'
+    }
+}
