3c448938d6588dfe3efb358af717c23fc087309c
meyta2020 <tmeysner@gmail.com>
2020-11-12 18:24:27 -0600

    Codewars solution "Sum Arrays"

---
+++ Sum Arrays.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata//53dc54212259ed3d4f00071c
+
+function sum (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+
+}
