68ab420c6f7123393c486f7efbd5a0e9fe3eb4fa
meyta2020 <tmeysner@gmail.com>
2020-11-14 13:03:13 -0600

    Codewars solution "Count the Monkeys!"

---
+++ Count the Monkeys!.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata//56f69d9f9400f508fb000ba7
+
+function monkeyCount(n) {
+    let arr= [];
+    for (let x = 1; x <= n; x++) {
+        arr.push(x);
+    }
+    return arr;
+}
