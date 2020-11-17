d925830c72b37bb2e57809180c50137093325c70
meyta2020 <tmeysner@gmail.com>
2020-11-17 10:38:32 -0600

    Codewars solution "Series of integers from m to n"

---
+++ Series of integers from m to n.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata//5841f680c5c9b092950001ae
+
+function generateIntegers(m, n) {
+    let arr = [];
+    for (let x = m; x <= n; x++) {
+        arr.push(x);
+    }
+    return arr;
+}
