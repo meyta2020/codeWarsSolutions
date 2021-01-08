bc64f9e35f7cccfffd22cc55760738dfa7512cf1
meyta2020 <tmeysner@gmail.com>
2021-01-07 21:00:34 -0600

    Codewars solution "Complete the matrix pattern"

---
+++ Complete the matrix pattern.js
@@ -0,0 +1,27 @@
+// https://www.codewars.com/kata//582c01ad3fd1cc5736000348
+
+function makeMatrix(m,n){
+    let digits = String(m);
+    let arr =Array(n);
+    for (let i = 0; i < n; i++) {
+        arr[i] = Array(n);
+    }
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (i === j || i + j === n - 1) {
+                arr[i][j] = +digits[0];
+            }
+            else if (i < j && i + j < n - 1) {
+                arr[i][j] = +digits[1];
+            } else if (i < j && i + j > n -1) {
+                arr[i][j] = +digits[4];
+            } else if (i > j && i + j > n -1){
+                arr[i][j] = +digits[2];
+            } else {
+                arr[i][j] = +digits[3];
+            }
+        }
+    }
+    arr = arr.map(el => el.join(' '));
+    return arr.join('\n');
+}
